c1a2c0522ddc8a720cffe13c0b216ef16a7065a5
angck <zenta@live.cn>
2023-05-30 22:37:20 +0800

    chore(docs): 添加文档

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV !== 'production' ? "" : "/pack-qrcode/demo/",
+  publicPath: process.env.NODE_ENV !== 'production' ? "" : "/qr-code/demo/",
   outputDir: "demo",
 })
